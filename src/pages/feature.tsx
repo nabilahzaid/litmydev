d31b29cde8c72823abe2187ef2135cc78415833d
mimmy <47461474+mimmydev@users.noreply.github.com>
2023-12-03 22:47:35 +0800

    feat: Add feature page

---
+++ src/pages/feature.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Feature() {
+  return (
+    <div className="feat">
+      <h1>i do something hahah</h1>
+    </div>
+  );
+}
+
+export default Feature;
