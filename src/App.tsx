f5416b3c13f15ff2ce3070e3f9435f13cac753f0
nabilahzaid <nabilah.zaid@gmail.com>
2024-03-17 11:25:06 +0800

    remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-import { ChakraProvider, Heading } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import BrowserRouters from "./routes/BrowserRouters";
 
 function App() {
